861fd02aa0b85e2f135878921b97bb3793364ead
jiajun mac <1009839598@qq.com>
2023-12-05 23:06:21 -0600

    fix

--- myapp/views.py
+++ myapp/views.py
@@ -108,7 +108,7 @@ def inserttrackable(request):
                 "INSERT INTO Trackable (user_id, checkin_date, tag_id, condition_id, weather_id, symptom_id, treatment_id) "
                 "VALUES (%s, %s, %s, %s, %s, %s, %s)",
                 [user_id, checkin_date, tag_id, condition_id, weather_id, symptom_id, treatment_id])
-        return redirect('/personal')
+        return redirect(f'/personal/{user_id}')
     else:
         return render(request, 'inserttrackable.html')
 
@@ -152,9 +152,8 @@ def personal(request, user_id):
                 """, [trackable_id])
         elif delete_action == 'user':
             with connection.cursor() as cursor:
-                cursor.execute("""
-                    delete from UserInfo where user_id = %s
-                """, [user_id])
+                cursor.execute("delete from Trackable where user_id = %s;", [user_id])
+                cursor.execute("delete from UserInfo where user_id = %s;", [user_id])
             return redirect('/login')
 
     with connection.cursor() as cursor:
@@ -229,7 +228,7 @@ def register(request):
                 "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, 2)",
                 [user_id, username, email, phone, password, gender, country, age])
 
-        return render(request, 'register.html', {'success': 'Register successfully!'})
+        return redirect('/login')
     else:
         return render(request, 'register.html')
 
@@ -411,16 +410,15 @@ def square(request):
             cursor.execute("""  
                 CREATE PROCEDURE GetUserInfoBySymptom(IN symptom_name VARCHAR(255))
                 BEGIN
-                    DECLARE symptomId INT;
-
-                    SELECT symptom_id INTO symptomId
-                    FROM Symptom
-                    WHERE name = symptom_name;
-
-                    SELECT u.username, u.age, u.gender, u.country, u.email, u.phone
+                    SELECT u.username, u.age, u.gender, u.country, u.email, u.phone, tre.name, tre.value
                     FROM UserInfo u
                     LEFT JOIN Trackable t ON u.user_id = t.user_id
-                    WHERE t.symptom_id = symptomId;
+                    LEFT JOIN Treatment tre ON t.treatment_id = tre.treatment_id
+                    WHERE t.symptom_id IN (
+                        SELECT symptom_id
+                        FROM Symptom
+                        WHERE name = symptom_name
+                    );
                 END;
             """)
             connection.commit()
